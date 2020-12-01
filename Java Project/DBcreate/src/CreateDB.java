d33925a6992dee53004afe5a0b7854e97de44d47
Jotter-Vortex <ckdwls525@gmail.com>
2020-12-01 22:31:48 +0900

    Create Database 수정

--- Java Project/DBcreate/src/CreateDB.java
+++ Java Project/DBcreate/src/CreateDB.java
@@ -40,104 +40,180 @@ public class CreateDB {
 	}
 	
 	public static void createTable(Connection conn, Statement stmt) {
-		// Fill out your code.
-		String sql = ""; // an SQL statement
+		String sql = ""; 			// an SQL statement
 		String table_parsing = "";	// 테이블 String에서 $값을 없앤 결과를 담을 변수 
-		String value = "";	// txt 파일에서 읽어들인 값을 저장할 변수 
-		String[] value_parsing;	// value에 저장된 String을 파싱한 결과를 담을 변수 
+		String value = "";			// txt 파일에서 읽어들인 값을 저장할 변수 
+		String[] value_parsing;		// value에 저장된 String을 파싱한 결과를 담을 변수 
+		String[] thereistable;
+		thereistable = new String[13];
+		int cnt=0;
+		int res;
+		String check;
 		
 		// Execute an SQL statement for CREATE TABLE
 		try {
 			conn.setAutoCommit(false); // auto-commit disabled  
 			// Create a statement object
 			stmt = conn.createStatement();
-			int res;
+			
+			
+			// 13개의 table 삭
+//			sql = "DROP TABLE " + "PEOPLE" + " CASCADE;";
+//			sql += "DROP TABLE " + "CATEGORY" + " CASCADE;";
+//			sql += "DROP TABLE " + "EVENT" + " CASCADE;";
+//			sql += "DROP TABLE " + "USER_DATA" + " CASCADE;";
+//			sql += "DROP TABLE " + "EXPERT_DATA" + " CASCADE;";
+//			sql += "DROP TABLE " + "PAYMENT" + " CASCADE;";
+//			sql += "DROP TABLE " + "SEND" + " CASCADE;";
+//			sql += "DROP TABLE " + "EMAKE" + " CASCADE;";
+//			sql += "DROP TABLE " + "CMAKE" + " CASCADE;";
+//			sql += "DROP TABLE " + "CRECORD" + " CASCADE;";
+//			sql += "DROP TABLE " + "ERECORD" + " CASCADE;";
+//			sql += "DROP TABLE " + "MANAGE" + " CASCADE;";
+//			sql += "DROP TABLE " + "WRITE" + " CASCADE;";
+//			res = stmt.executeUpdate(sql); 
+			
 			// Let's execute an SQL statement.
-			// 만약 pgAdmin에 이미 테이블이 있다면 해당 코드를 사용해서 제거하고 다시 CREATE합니다.
-			// DB에 11개의 table이 존재하지 않는다면 주석처리 해주세요 
-			sql = "DROP TABLE " + "PEOPLE" + " CASCADE;";
-			sql += "DROP TABLE " + "CATEGORY" + " CASCADE;";
-			sql += "DROP TABLE " + "EVENT" + " CASCADE;";
-			sql += "DROP TABLE " + "USER_DATA" + " CASCADE;";
-			sql += "DROP TABLE " + "EXPERT_DATA" + " CASCADE;";
-			sql += "DROP TABLE " + "PAYMENT" + " CASCADE;";
-			sql += "DROP TABLE " + "SEND" + " CASCADE;";
-			sql += "DROP TABLE " + "MAKE" + " CASCADE;";
-			sql += "DROP TABLE " + "RECORD" + " CASCADE;";
-			sql += "DROP TABLE " + "MANAGE" + " CASCADE;";
-			sql += "DROP TABLE " + "WRITE" + " CASCADE;";
-			//System.out.println(sql);
+			// 만약 pgAdmin에 테이블이 없다면 테이블을 생성합니다. 
+			DatabaseMetaData metadata = conn.getMetaData();
+			ResultSet resultSet;
+			resultSet = metadata.getTables(null, null, "%", null);
+			while(resultSet.next()) {
+				check = resultSet.getString("TABLE_NAME");
+				//System.out.println(check);
+				if(check.equals("people")) thereistable[cnt++]="people";
+				if(check.equals("user_data")) thereistable[cnt++]="user_data";
+				if(check.equals("expert_data")) thereistable[cnt++]="expert_data";
+				if(check.equals("category")) thereistable[cnt++]="category";
+				if(check.equals("event")) thereistable[cnt++]="event";
+				if(check.equals("cmake")) thereistable[cnt++]="cmake";
+				if(check.equals("emake")) thereistable[cnt++]="emake";
+				if(check.equals("payment")) thereistable[cnt++]="payment";
+				if(check.equals("manage")) thereistable[cnt++]="manage";
+				if(check.equals("crecord")) thereistable[cnt++]="crecord";
+				if(check.equals("erecord")) thereistable[cnt++]="erecord";
+				if(check.equals("send")) thereistable[cnt++]="send";
+				if(check.equals("write")) thereistable[cnt++]="write";
+			}
+			
+			System.out.println(sql);
 			res = stmt.executeUpdate(sql); 
-			if(res == 0) 
-				System.out.println("Table was successfully dropped.");
+
 			StringBuffer sb = new StringBuffer();
-			// 11개의 TABLE을 생성합니다. 
+			
+			// 13개의 TABLE을 생성합니다. 
+			if(!Isintable(thereistable, "people", cnt)) {
 			sb.append("CREATE TABLE PEOPLE(\n" + 
-					"	Name character varying(25) NOT NULL,\n" + 
 					"	Id character varying(25) NOT NULL,\n" + 
-					"	pw character varying(25) NOT NULL,\n" + 
+					"	Pw character varying(25) NOT NULL,\n" +
+					"	Name character varying(25) NOT NULL,\n" + 
 					"	Sex  character varying(8) check (Sex in ('남','여')),\n" + 
-					"	Type character varying(5),\n" + 
+					"	Phone character varying(15),\n" +
 					"	Birth Date,\n" + 
-					"primary key(ID))"+ ";");
+					"	Type character varying(7) NOT NULL,\n" + 
+					"primary key(Id))"+ ";");
+			}
+			if(!Isintable(thereistable, "user_data", cnt)) {
+				sb.append("CREATE TABLE USER_DATA(\n" + 
+						"	Uindex integer,\n" + 
+						"	Date Date,\n" +
+						"	Detail text,\n" + 
+						"primary key(Uindex));");
+				}
+			if(!Isintable(thereistable, "expert_data", cnt)) {
+				sb.append("CREATE TABLE EXPERT_DATA(\n" + 
+						"	Eindex integer,\n" + 
+						"	Ecomment text,\n" + 
+						"	Emergency character varying(2) check (Emergency in ('1','0')),\n" + 
+						"primary key(Eindex));");
+				}
+			if(!Isintable(thereistable, "category", cnt)) {
 			sb.append("CREATE TABLE CATEGORY(\n" + 
 					"	Cnum integer,\n" + 
 					"	Cname character varying(30),\n" + 
 					"primary key(Cnum)" + ");");
+			}
+			if(!Isintable(thereistable, "event", cnt)) {
 			sb.append("CREATE TABLE EVENT(\n" + 
 					"	Enum integer,\n" + 
-					"	Eame character varying(30),\n" + 
+					"	Ename character varying(30),\n" + 
 					"	Etype character varying(5),\n" + 
 					"primary key(Enum)" + ");");
-			sb.append("CREATE TABLE USER_DATA(\n" + 
-					"	Uindex integer,\n" + 
-					"	Date Date,\n" +
-					"	Detail text,\n" + 
-					"primary key(Uindex));");
-			sb.append("CREATE TABLE EXPERT_DATA(\n" + 
-					"	Eindex integer,\n" + 
-					"	EComment text,\n" + 
-					"	Emergency character varying(2) check (Emergency in ('1','0')),\n" + 
-					"primary key(Eindex));");
-			sb.append("CREATE TABLE PAYMENT(\n" + 
-					"	payTF character varying(2) check (payTF in ('1','0')),\n" +
-					"	UId character varying(25) NOT NULL,\n" +
-					"	EId character varying(25) NOT NULL" +
-					");");
-			sb.append("CREATE TABLE SEND(\n" + 
-					"	Ucomment text,\n" +
-					"	Important boolean\n" +
-					");");
-			sb.append("CREATE TABLE MAKE(\n" + 
-					"	UId character varying(25) NOT NULL,\n" +
-					"	num integer\n" +
-					");");
-			sb.append("CREATE TABLE RECORD(\n" + 
+			}
+			if(!Isintable(thereistable, "cmake", cnt)) {
+				sb.append("CREATE TABLE CMAKE(\n" + 
+						"	Uid character varying(25) NOT NULL,\n" +
+						"	Cnum integer,\n" +
+						"primary key(Uid, Cnum)" + ");");
+				}
+			if(!Isintable(thereistable, "emake", cnt)) {
+				sb.append("CREATE TABLE EMAKE(\n" + 
+						"	Uid character varying(25) NOT NULL,\n" +
+						"	Enum integer,\n" +
+						"primary key(Uid, Enum)" + ");");
+				}
+			if(!Isintable(thereistable, "payment", cnt)) {
+			sb.append("CREATE TABLE PAYMENT(\n" +
+					"	Uid character varying(25) NOT NULL,\n" +
+					"	Eid character varying(25) NOT NULL,\n" +
+					"	PayTF character varying(2) check (payTF in ('1','0')),\n" +
+					"primary key(Uid, Eid)" + ");");
+			}
+			if(!Isintable(thereistable, "manage", cnt)) {
+				sb.append("CREATE TABLE MANAGE(\n" + 
+						"	Uid character varying(25) NOT NULL,\n" +
+						"	Uindex integer,\n" +
+						"primary key(Uid, Uindex)" + ");");
+				}
+			if(!Isintable(thereistable, "crecord", cnt)) {
+				sb.append("CREATE TABLE CRECORD(\n" +
+						"	Cnum integer,\n" +
+						"	Uindex integer,\n" +
+						"primary key(Cnum, Uindex)" + ");");
+				}
+			if(!Isintable(thereistable, "erecord", cnt)) {
+				sb.append("CREATE TABLE ERECORD(\n" + 
+						"	Enum integer,\n" +
+						"	Uindex integer,\n" +
+						"primary key(Enum, Uindex)" + ");");
+				}	
+			if(!Isintable(thereistable, "send", cnt)) {
+			sb.append("CREATE TABLE SEND(\n" +
 					"	Uindex integer,\n" +
-					"	num integer\n" +
-					");");
-			sb.append("CREATE TABLE MANAGE(\n" + 
-					"	UId character varying(25) NOT NULL,\n" +
-					"	Uindex integer\n" +
-					");");
+					"	Eindex integer,\n" +
+					"	Ucomment text,\n" +
+					"primary key(Uindex, Eindex)" + ");");
+				}
+			if(!Isintable(thereistable, "write", cnt)) {
 			sb.append("CREATE TABLE WRITE(\n" + 
-					"	UId character varying(25) NOT NULL,\n" +
-					"	Eindex integer\n" +
-					");");
-			System.out.println(sb);
-			sql = sb.toString();
-			//System.out.println(sql);
-			// Try 'CREATE TABLE ...'
-			res = stmt.executeUpdate(sql); 
-			if(res == 0) 
-				System.out.println("Table was successfully created.");
-			// Make the table permanently stored by a commit.
-			conn.commit();			
+					"	Eid character varying(25) NOT NULL,\n" +
+					"	Eindex integer,\n" +
+					"primary key(Eid, Eindex)" + ");");
+				}
+			
+			if(sb!=null) {
+				System.out.println(sb);
+				sql = sb.toString();
+				res = stmt.executeUpdate(sql); 
+				if(res == 0) 
+					System.out.println("Table was successfully created.");
+				conn.commit();			
+			}
 
-		}catch(SQLException ex2) {
+		} catch(SQLException ex2) {
 			System.err.println("sql error = " + ex2.getMessage());
 			System.exit(1);
 		}
 
+	}
+	public static boolean Isintable(String[] table, String target, int n) {
+		int i;
+		
+		for (i=0; i<n; i++) {
+			if(table[i].equals(target)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
