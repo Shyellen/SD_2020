ed174240eeca542d037b356b32d923011473ff53
Jotter-Vortex <ckdwls525@gmail.com>
2020-12-05 19:49:33 +0900

    insert checkPayment in payment.java

--- Java Project/Health Care SW/src/GUI/Payment.java
+++ Java Project/Health Care SW/src/GUI/Payment.java
@@ -7,7 +7,33 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Payment {
-	public static boolean paymentRequest(Connection conn, Statement stmt, String ID) {	// 해당 ID로 생성된 카테고리 수를 확인한다.
+	
+	public static boolean checkPayment(Connection conn, Statement stmt, String ID) {	//지불여부를 반환함 
+		ResultSet rs = null;
+		String DBcnt = "";
+		String sql="";
+		int check=0;
+		String type = LoginProcess.checkType(conn,stmt,ID);
+		try {
+			sql = "SELECT payTF FROM PAYMENT"
+					+ "WHERE EID = (SELECT ID FROM PEOPLE WHERE ID = "+ ID +");";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			int i = 0;
+			while(rs.next()) {
+				check = rs.getInt(1);
+			}
+			rs.close();
+		}
+		catch (SQLException e) {
+			System.err.println("sql error = " + e.getMessage());
+//			System.exit(1);
+		}
+		if(check==1) return true;
+		else return false;
+	}
+	
+	public static boolean paymentRequest(Connection conn, Statement stmt, String ID) {	//지불여부를 반환함 
 		ResultSet rs = null;
 		String DBcnt = "";
 		String sql="";
@@ -30,9 +56,6 @@ public class Payment {
 			System.err.println("sql error = " + e.getMessage());
 //			System.exit(1);
 		}
-		if(check==true)
-			return true;
-		else
-			return false;
+		return check;
 	}
 }
